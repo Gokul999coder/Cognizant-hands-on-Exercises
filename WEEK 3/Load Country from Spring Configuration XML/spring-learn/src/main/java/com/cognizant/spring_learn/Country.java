f288a7daaa7715ce493731ad65b5662dab02a9b2
Gokul999coder <itsgokul555@gmail.com>
2026-07-06 23:26:21 +0530

    module 7 completed 1,2

---
+++ WEEK 3/Load Country from Spring Configuration XML/spring-learn/src/main/java/com/cognizant/spring_learn/Country.java
@@ -0,0 +1,49 @@
+package com.cognizant.spring_learn;
+
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
+
+public class Country {
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(Country.class);
+    private String code;
+    private String name;
+
+    public Country(){
+        LOGGER.debug("Inside Country Constructor.");
+    }
+
+    public void setCode(String code)
+    {
+        LOGGER.debug("Inside setCode()");
+        this.code=code;
+    }
+
+    public String getCode()
+    {
+        LOGGER.debug("Inside getCode()");
+        return code;
+    }
+
+    public void setName(String name)
+    {
+        LOGGER.debug("Inside setName()");
+        this.name=name;
+    }
+
+    public String getName()
+    {
+        LOGGER.debug("Inside getName()");
+        return name;
+    }
+    @Override
+    public String toString(){
+        return "Country{" +
+                "code='" + code + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+
+}
---
+++ WEEK 3/Load Country from Spring Configuration XML/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
@@ -0,0 +1,28 @@
+package com.cognizant.spring_learn;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+@SpringBootApplication
+public class SpringLearnApplication {
+	private static final Logger LOGGER= LoggerFactory.getLogger(SpringLearnApplication.class);
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLearnApplication.class, args);
+		System.out.println("Main started");
+		displayCountry();
+	}
+	private static void displayCountry(){
+
+		LOGGER.debug("start");
+		ApplicationContext context= new  ClassPathXmlApplicationContext("Country.xml");
+		Country country=context.getBean("country",Country.class);
+		LOGGER.debug("Country : {}",country.toString());
+		LOGGER.debug("END");
+
+	}
+
+}
